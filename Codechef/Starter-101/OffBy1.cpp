233624388df4dfe0da00423cb575239134c0d021
Parmeet Singh <keepitok.200@gmail.com>
2023-09-21 13:46:16 +0530

    Create OffBy1.cpp
    Solved problem from codechef live contest

---
+++ Codechef/Starter-101/OffBy1.cpp
@@ -0,0 +1,16 @@
+#include<bits/stdc++.h>
+using namespace std;
+typedef long long ll;
+
+void solve() {
+	int a,b;
+	cin >> a >> b;
+	int ans = (a+b)*10;
+	cout << ans + 1 << "\n";
+}
+int main() {
+	ios::sync_with_stdio(false);
+	cin.tie(NULL);
+	//int tt; cin >> tt; for(int i = 0; i < tt; i++) {solve();}
+	solve();
+}
